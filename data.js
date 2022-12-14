5a7166db0a6942405887d07abaed94fea7dd5c5d
Vdubsruleok <robert.weidinger@gmail.com>
2022-12-14 20:02:18 +1100

    CCAMLR Visitors Tour
    Visitors tour restricted to the Conference  Rooms and Chapel.

---
+++ data.js
@@ -0,0 +1,372 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-top-carpark",
+      "name": "Top Carpark",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -1.7900662959761462,
+        "pitch": -0.3181323508319043,
+        "fov": 1.4080178966706975
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6957027635330384,
+          "pitch": 0.07187872259433448,
+          "rotation": 0,
+          "target": "1-admin-reception"
+        },
+        {
+          "yaw": -2.796308883252209,
+          "pitch": -0.02503153286645876,
+          "rotation": 13.351768777756625,
+          "target": "5-bottom-entrance"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-admin-reception",
+      "name": "Admin Reception",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.6526441707802135,
+        "pitch": 0.05849784949634795,
+        "fov": 1.4080178966706975
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.825228270676126,
+          "pitch": 0.16277969535845038,
+          "rotation": 4.71238898038469,
+          "target": "0-top-carpark"
+        },
+        {
+          "yaw": 1.1375317952882327,
+          "pitch": -0.6621596398227876,
+          "rotation": 5.497787143782138,
+          "target": "2-conference-room---front"
+        },
+        {
+          "yaw": 0.26512952215039753,
+          "pitch": 0.01272688918031406,
+          "rotation": 4.71238898038469,
+          "target": "6-wombat-room---rear"
+        },
+        {
+          "yaw": 1.1294359879781446,
+          "pitch": 0.02900896870408154,
+          "rotation": 7.853981633974483,
+          "target": "2-conference-room---front"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-conference-room---front",
+      "name": "Conference Room - Front",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -2.0150676503055642,
+        "pitch": 0.08791341186412183,
+        "fov": 1.4080178966706975
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0908536075249238,
+          "pitch": -0.06100998114102296,
+          "rotation": 3.141592653589793,
+          "target": "3-conference-room---rear"
+        },
+        {
+          "yaw": 2.9426366939995345,
+          "pitch": 0.081992102575839,
+          "rotation": 6.283185307179586,
+          "target": "4-chapel"
+        },
+        {
+          "yaw": -2.5310638313383276,
+          "pitch": 0.015753151010162725,
+          "rotation": 7.853981633974483,
+          "target": "6-wombat-room---rear"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.9912331024958476,
+          "pitch": -0.2403775808340054,
+          "title": "Conference Room",
+          "text": "Front"
+        }
+      ]
+    },
+    {
+      "id": "3-conference-room---rear",
+      "name": "Conference Room - Rear",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9888799045256178,
+          "pitch": -0.16361071851387265,
+          "rotation": 3.141592653589793,
+          "target": "2-conference-room---front"
+        },
+        {
+          "yaw": -0.4397545482408809,
+          "pitch": 0.029143123399624216,
+          "rotation": 1.5707963267948966,
+          "target": "4-chapel"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-chapel",
+      "name": "Chapel",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -2.7228392854779937,
+        "pitch": 0.15229929247482232,
+        "fov": 1.4080178966706975
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1359123634655681,
+          "pitch": 0.16981683450362084,
+          "rotation": 0,
+          "target": "1-admin-reception"
+        },
+        {
+          "yaw": 0.22640707230664958,
+          "pitch": 0.09919037573532208,
+          "rotation": 0,
+          "target": "2-conference-room---front"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-bottom-entrance",
+      "name": "Bottom Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -2.6863503975875194,
+        "pitch": 0.12731768280155187,
+        "fov": 1.4080178966706975
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1179034682054834,
+          "pitch": 0.15694964389382804,
+          "rotation": 4.71238898038469,
+          "target": "0-top-carpark"
+        },
+        {
+          "yaw": -2.6492660491937805,
+          "pitch": 0.1302946482199836,
+          "rotation": 1.5707963267948966,
+          "target": "6-wombat-room---rear"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-wombat-room---rear",
+      "name": "Wombat Room - Rear",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.3374465429609472,
+        "pitch": 0.12208265185178391,
+        "fov": 1.4080178966706975
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.810525238211639,
+          "pitch": 0.04532091039198782,
+          "rotation": 4.71238898038469,
+          "target": "5-bottom-entrance"
+        },
+        {
+          "yaw": -2.2751015045528025,
+          "pitch": 0.10969856007365486,
+          "rotation": 5.497787143782138,
+          "target": "3-conference-room---rear"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
